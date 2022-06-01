d0a0141c132c0b6addd08641285f842a28761465
cesartidrick <cesartidrick@gmail.com>
2022-06-01 13:58:15 -0600

    first commits to Recipes

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import RecipeList from "./RecipeList.js"
+import RecipeCard from "./RecipeCard.js"
+import NewRecipeForm from "./NewRecipeForm"
 
 function Recipes() {
-  return ;
+ 
+  return (
+    <div>
+      <RecipeList/> 
+
+      <RecipeCard/>
+
+      <NewRecipeForm/>
+    </div>
+   
+  );
 }
 
 export default Recipes;
