2d9db93ffc933c7a2bf4df904c6e11570ce24639
Eli Craig <elidcraig@gmail.com>
2022-06-01 09:45:50 -0700

    outline for toastimonials component

--- src/components/Toastimonials.js
+++ src/components/Toastimonials.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Toastimonials() {
-  return ;
+  const [toastimonials, setToastimonials] = useState([])
+  
+  useEffect(() => {
+    fetch('http://localhost:4000/toastimonials')
+      .then(resp => resp.json())
+      .then(data => setToastimonials(data))
+  }, [])
+
+  return (
+    <div className='toastimonials'>
+
+    </div>
+  );
 }
 
 export default Toastimonials;
