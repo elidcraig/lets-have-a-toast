6e6e0af8cf2b29ca2cfb9e149d8bde513333a75e
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-06-01 11:53:45 -0600

    random toast generator draft code

--- src/components/RandomDisplay.js
+++ src/components/RandomDisplay.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function RandomDisplay() {
-  return ;
+  const [toastLevel, setToastLevel] = useState('Golden');
+  const [secondTopping, setSecondTopping] = useState('PB');
+  const [firstTopping, setFirstTopping] = useState('fresh fruit');
+  const [bread, setBread] = useState('multigrain');
+
+  function randomSelector() {}
+
+  function generateToastLevel() {
+    useEffect(() => {
+      fetch('http://localhost:3000/toastLevel')
+        .then((response) => response.json())
+        .then(setToastLevel);
+    }, []);
+  }
+
+  function generateSecondTopping() {
+    useEffect(() => {
+      fetch('http://localhost:3000/toastLevel')
+        .then((response) => response.json())
+        .then(setSecondTopping);
+    }, []);
+  }
+
+  function generateFirstTopping() {
+    useEffect(() => {
+      fetch('http://localhost:3000/toastLevel')
+        .then((response) => response.json())
+        .then(setFirstTopping);
+    }, []);
+  }
+
+  function generateBread() {
+    useEffect(() => {
+      fetch('http://localhost:3000/bread')
+        .then((response) => response.json())
+        .then(setBread);
+    }, []);
+  }
+
+  return (
+    <div className='randomDisplay'>
+      <button onClick={generateToastLevel}>Toast Level</button>
+      <span>{toastLevel}</span>
+      <button onClick={generateSecondTopping}>Topping #2</button>
+      <button onClick={generateFirstTopping}>Topping #1</button>
+      <button onClick={generateBread}>Bread</button>
+    </div>
+  );
 }
 
 export default RandomDisplay;
--- src/pages/RandomToast.js
+++ src/pages/RandomToast.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import RandomToastForm from '../components/RandomToastForm';
+import RandomDisplay from '../components/RandomDisplay';
 
 function RandomToast() {
-  return ;
+  return;
 }
 
 export default RandomToast;
