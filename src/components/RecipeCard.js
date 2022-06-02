f14045c2210d5ec7e6c0d411ae4946cf33e7834c
Eli Craig <elidcraig@gmail.com>
2022-06-02 11:13:06 -0700

    logo on cards

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './RecipeCard.css'
+// import logo from '../../public/toast-logo.png'
 
 function RecipeCard({name, recipeDetails, notes }) {
   return (
     <li className="recipe-card">
       <h4>{name}</h4>
+      <div className="recipe-card__img-wrapper">
+      <img src='../toast-logo.png' alt='toast logo' />
+      </div>
       <p>Recipe Details: {recipeDetails}</p>
       <p>Notes: {notes}</p>
   </li>
