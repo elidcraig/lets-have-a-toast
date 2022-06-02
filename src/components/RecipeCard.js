0813bac8f1fff06c917c28859b14b6a2ab663a44
Eli Craig <elidcraig@gmail.com>
2022-06-02 11:39:31 -0700

    cards scale on hover

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -5,14 +5,14 @@ import './RecipeCard.css'
 
 function RecipeCard({name, recipeDetails, notes }) {
   return (
-    <li className="recipe-card">
+    <motion.li className="recipe-card" whileHover={{ scale: 1.05 }}>
       <h4>{name}</h4>
       <div className="recipe-card__img-wrapper">
       <img src='../toast-logo.png' alt='toast logo' />
       </div>
       <p>Recipe Details: {recipeDetails}</p>
       <p>Notes: {notes}</p>
-  </li>
+  </motion.li>
   );
 }
 
